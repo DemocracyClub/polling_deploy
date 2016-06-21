5e938bb99b4ac379282176d5dda0d9aa4cb9a33c
Ash Berlin <ash_github@firemirror.com>
2016-06-21 09:17:12 +0100

    Hit Django app for ELB healthcheck
    We only want to be added to the loadbalancer pool once django is ready
    to serve requests.
    
    ELBs don't sent a domain name in the Host: header but the IP address of
    the instance - this coupled with Django in non-debug mode means we have
    to add the instance IP to the ALLOWED_HOSTS setting

--- webapp_settings/production.py
+++ webapp_settings/production.py
@@ -1,4 +1,5 @@
 from .base import *
+import socket
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
@@ -29,7 +30,10 @@ DATABASE_ROUTERS = ['polling_stations.db_routers.LoggerRouter',]
 
 STATIC_PRECOMPILER_DISABLE_AUTO_COMPILE = True
 
-ALLOWED_HOSTS = [
+# We need to also respond to the private IP address of the instance as that's
+# what the ELB will send healthcheck requests to
+local_ip_addresses = [(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close())[1] for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]]
+ALLOWED_HOSTS = local_ip_addresses + [
     {% for domain in domains %}
     "{{ domain}}",
     {% endfor %}
