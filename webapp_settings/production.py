1e27e9bc9fa7241c158a58b965b30fec1fd4cd15
Sym Roe <sym.roe@talusdesign.co.uk>
2017-03-24 10:21:03 +0000

    Lots of changes, sorry

--- webapp_settings/production.py
+++ webapp_settings/production.py
@@ -5,7 +5,7 @@ DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    ('Sym Roe', 'sym+polling@democracyclub.org.uk'),
+    ('DC developers', 'developers@democracyclub.org.uk'),
 )
 
 DATABASES = {
@@ -17,20 +17,25 @@ DATABASES = {
         'HOST': '',
         'PORT': '',
     },
-    'logger': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'logger_prod',
-        'USER': 'polling_stations',
-        'PASSWORD': '{{ logger_prod_password }}',
-        'HOST': 'logger-prod.c0jvejtwfveq.eu-west-1.rds.amazonaws.com',
-        'PORT': '5432',
-    }
 }
-DATABASE_ROUTERS = ['polling_stations.db_routers.LoggerRouter',]
 
+{% if use_logger %}
+DATABASES['logger'] = {
+    'ENGINE': 'django.contrib.gis.db.backends.postgis',
+    'NAME': 'logger_prod',
+    'USER': 'polling_stations',
+    'PASSWORD': '{{ logger_prod_password }}',
+    'HOST': 'logger-prod.c0jvejtwfveq.eu-west-1.rds.amazonaws.com',
+    'PORT': '5432',
+}
+
+DATABASE_ROUTERS = ['polling_stations.db_routers.LoggerRouter',]
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 STATIC_PRECOMPILER_DISABLE_AUTO_COMPILE = True
 STATIC_URL = 'https://s3-eu-west-1.amazonaws.com/pollingstations-assets/'
+
+{% endif %}
+
 
 # We need to also respond to the private IP address of the instance as that's
 # what the ELB will send healthcheck requests to
@@ -44,3 +49,9 @@ ALLOWED_HOSTS = local_ip_addresses + [
 {% if google_maps_api_key %}
 GOOGLE_API_KEY = '{{ google_maps_api_key }}'
 {% endif %}
+
+MORPH_API_KEY = '{{ vault_morph_api_key }}'
+MAPIT_URL = "http://mapit.democracyclub.org.uk/"
+MAPIT_UA = "scraper/sym"
+
+EXAMPLE_POSTCODE = "GU17 0BP"
