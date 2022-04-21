fd718b6c8db83db2a984635e9b8927d6095b777c
GeoWill <roper.william@gmail.com>
2022-04-21 10:27:11 +0100

    Fix EONI shell script

--- files/import_eoni_from_s3.sh
+++ files/import_eoni_from_s3.sh
@@ -3,10 +3,10 @@ set -e
 set -x
 
 
-LATEST_FILE=$(aws s3 ls s3://eoni-data.wheredoivote.co.uk/production_data/ | sort | tail -n1 | rev | cut -d' ' -f1 | rev)
+LATEST_FILE=$(/usr/local/bin/aws s3 ls s3://eoni-data.wheredoivote.co.uk/production_data/ | sort | tail -n1 | rev | cut -d' ' -f1 | rev)
 SRCDIR='/tmp/eoni_production_data'
 rm -rf $SRCDIR && mkdir -p $SRCDIR
 
-aws s3 cp s3://eoni-data.wheredoivote.co.uk/production_data/"${LATEST_FILE}" $SRCDIR
+/usr/local/bin/aws s3 cp s3://eoni-data.wheredoivote.co.uk/production_data/"${LATEST_FILE}" $SRCDIR
 
-/var/www/polling_stations/code/env/bin/python /var/www/polling_stations/code/manage.py import_eoni --cleanup $SRCDIR/"${LATEST_FILE}"
+/var/www/polling_stations/env/bin/python /var/www/polling_stations/code/manage.py import_eoni --cleanup $SRCDIR/"${LATEST_FILE}"
