f12f86b70d84594b224438d4cfca960c36d47180
GeoWill <roper.william@gmail.com>
2022-04-25 13:09:32 +0100

    Use cs2cs to reproject EONI data

--- files/import_eoni_from_s3.sh
+++ files/import_eoni_from_s3.sh
@@ -9,4 +9,18 @@ rm -rf $SRCDIR && mkdir -p $SRCDIR
 
 /usr/local/bin/aws s3 cp s3://eoni-data.wheredoivote.co.uk/production_data/"${LATEST_FILE}" $SRCDIR
 
-/var/www/polling_stations/env/bin/python /var/www/polling_stations/code/manage.py import_eoni --cleanup $SRCDIR/"${LATEST_FILE}"
+echo '"PREM_X_4326","PREM_Y_4326"' > prem_4326.csv
+mlr --icsv --otsv --headerless-csv-output cut -f PREM_X_COR,PREM_Y_COR $SRCDIR/"${LATEST_FILE}" |
+	cs2cs -f "%.6f" +init=epsg:29902 +to +init=epsg:4326 |
+	awk '{print "\""$1"\",\""$2"\""}' >> PREM_4326.csv
+
+echo '"PRO_X_4326","PRO_Y_4326"' > pro_4326.csv
+mlr --icsv --otsv --headerless-csv-output cut -f PRO_X_COR,PRO_Y_COR $SRCDIR/"${LATEST_FILE}" |
+	cs2cs -f "%.6f" +init=epsg:29902 +to +init=epsg:4326 |
+	awk '{print "\""$1"\",\""$2"\""}'  >> PRO_4326.csv
+
+paste -d ',' PRO_4326.csv PREM_4326.csv $SRCDIR/"${LATEST_FILE}" > eoni_reprojected.csv
+
+/var/www/polling_stations/env/bin/python /var/www/polling_stations/code/manage.py import_eoni --cleanup --reprojected eoni_reprojected.csv
+
+rm PREM_4326.csv PRO_4326.csv eoni_reprojected.csv
